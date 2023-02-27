dfdfc59140efe530e01a823577b4b2d6e10a3813
AlyssaD <pink_lime@live.com>
2023-02-27 10:27:08 -0500

    add mission page

---
+++ src/pages/Mission/Mission.jsx
@@ -0,0 +1,24 @@
+import styles from './Mission.module.css'
+
+const Mission = () => {
+  return (
+    <>
+    <main>
+      <div>
+        <div>
+          <h1>It's 2023. And in 2023, universal design matters.</h1>
+          <h3>That's why we created Beauty Beyond Barriers, a collection of our most accessible makeup and skincare products.</h3>
+        </div>
+        <img />
+      </div>
+      <h2>A COLLECTION OF OUR MOST ACCESSIBLE PRODUCTS.</h2>
+      <p>It is Estee Lauder's goal to create a setting in which people of disabilities have equal access to the beauty industry, and the company is dedicated to achieving this objective. The Beauty Beyond Barriers (BBB) collection is made from products that have been carefully selected with the intention of removing barriers in the world of cosmetics and making them accessible to people with disabilities.</p>
+      <div>
+        <h2>Accessibility Matters</h2>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default Mission
