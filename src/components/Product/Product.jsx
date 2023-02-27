6f744146d53b724777d70ec18679671e3aec5182
chenannchi <anchichenusa@gmail.com>
2023-02-27 14:04:12 -0500

    set up product component structure

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,18 @@
+import styles from './Product.module.css'
+
+const Product = ({ user }) => {
+  return (
+    <main className={styles.container}>
+      <img src='' alt='product' />
+      <div>Product Name</div>
+      <div>Description</div>
+      <div>
+        <div>Stars</div>
+        <div>Number of Reiviews</div>
+      </div>
+      <div>Price</div>
+    </main>
+  )
+}
+
+export default Product
--- src/pages/BBB/BBB.jsx
+++ src/pages/BBB/BBB.jsx
@@ -1,5 +1,6 @@
 import styles from './BBB.module.css'
 import { Link } from 'react-router-dom'
+import Product from '../../components/Product/Product'
 
 const BBB = ({ user }) => {
   return (
@@ -45,6 +46,20 @@ const BBB = ({ user }) => {
             Estee Lauder is committed to fostering an environment in which individuals with disabilities have equal access to the beauty industry. The Beauty Beyond Barriers (BBB) collection is comprised of hand-picked items that are aimed at lowering entry barriers to the world of cosmetics and making them more disability-friendly.
           </p>
         </div>
+      </div>
+      <div id={styles.second_section}>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+      <div id={styles.third_section}>
+
       </div>
     </main>
   )
