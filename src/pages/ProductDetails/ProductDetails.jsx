bb39dac022330d12e841627a6783dd086f8d96df
chenannchi <anchichenusa@gmail.com>
2023-03-07 13:15:09 -0500

    set up the template for product details page

---
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import styles from './ProductDetails.module.css'
+
+const ProductDetails = ({ user }) => {
+  return (
+    <main className={styles.container}>
+      
+    </main>
+  )
+}
+
+export default ProductDetails
