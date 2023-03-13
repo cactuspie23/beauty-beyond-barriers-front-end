cb419786701a3125e2d744fdd338e3593b31dba7
chenannchi <anchichenusa@gmail.com>
2023-03-13 12:05:11 -0400

    modify the styles of product card

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,17 +5,19 @@ const Product = ({ product }) => {
   return (
     <main className={styles.container}>
       <Link to={`/products/${product._id}`}>
-        <img src={product.imgUrl} alt='product' id={styles.product_img} />
-        <div id={styles.title}>
-          <div id={styles.name}>{product.name}</div>
-          <img src='../../images/universal-icon.png' alt='accessible-icon' id={styles.accessibility_icon} />
-        </div>
-        <div id={styles.description}>{product.shortDescription}</div>
-        <div id={styles.review}>
-          <div>Stars</div>
-          <div>Number of Reiviews</div>
-        </div>
-        <div id={styles.price}>${product.price}.00</div>
+        {/* <div > */}
+          <img src={product.imgUrl} alt='product' id={styles.product_img} />
+          <div id={styles.title}>
+            <div id={styles.name}>{product.name}</div>
+            <img src='../../images/universal-icon.png' alt='accessible-icon' id={styles.accessibility_icon} />
+          </div>
+          <div id={styles.description}>{product.shortDescription}</div>
+          <div id={styles.review}>
+            <div>Stars</div>
+            <div>Number of Reiviews</div>
+          </div>
+          <div id={styles.price}>${product.price}.00</div>
+        {/* </div> */}
       </Link>
     </main>
   )
