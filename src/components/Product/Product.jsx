d4de10406f44a2df04afd5bb2674e05cee82d89d
AlyssaD <pink_lime@live.com>
2023-03-15 18:32:30 -0400

    make all products five stars

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,15 +2,6 @@ import styles from './Product.module.css'
 import { Link } from 'react-router-dom'
 
 const Product = ({ product }) => {
-  const getReviewNum = () => {
-    let stars = []
-    for (let i = 0; i < Math.floor(Math.random() * 5) + 1; i++) {
-      stars.push(<img src='/images/star.png' alt='star' key={i} />)
-    }
-    return stars
-  }
-
-
   return (
     <Link to={`/products/${product._id}`}>
       <main className={styles.container}>
@@ -23,9 +14,11 @@ const Product = ({ product }) => {
           </div>
           <div id={styles.review}>
             <div id={styles.stars}>
-              {
-                getReviewNum()
-              }
+              <img src='/images/star.png' alt='star' />
+              <img src='/images/star.png' alt='star' />
+              <img src='/images/star.png' alt='star' />
+              <img src='/images/star.png' alt='star' />
+              <img src='/images/star.png' alt='star' />
             </div>
             <div>({Math.floor(Math.random() * 1000)})</div>
           </div>
